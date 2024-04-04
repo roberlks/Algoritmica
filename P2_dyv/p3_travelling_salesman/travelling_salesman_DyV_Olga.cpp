c5b78de56999257217e994dfe20ce6ab0e385afe
Olga <cchengcheng2004@gmail.com>
2024-04-04 10:27:59 +0200

    Debugs

--- P2_dyv/p3_travelling_salesman/travelling_salesman_DyV_Olga.cpp
+++ P2_dyv/p3_travelling_salesman/travelling_salesman_DyV_Olga.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include "City.h"
 #include <list>
-#include <utility>
+#include <algorithm>
 
 using namespace std;
 
 typedef long long ll;
 
+/**
+ * @struct Auxiliar 
+ * @details Almacena parejas de iteradores constantes de list<int> que servirán de auxiliares en 
+ * la resolución del problema
+*/
 struct Auxiliar{
 
     typename list<int>::const_iterator it1;
@@ -27,16 +32,26 @@ struct Auxiliar{
     }
 };
 
+/**
+ * @brief Operador de salida, pinta una lista de int
+*/
 ostream& operator<<(ostream& os,const list<int>& l);
 
-// Calcula los puntos con la distancia mínima entre dos conjuntos
+/**
+ * @brief Calcula los puntos, uno de cada ciclo, cuya distancia sea mínima
+ * @param v Vector de City, donde se almacena información relativa a las ciudades
+ * @param c1 Lista donde guardan los índices de los elementos de un subconjunto de v
+ * @param c2 Lista donde guardan los índices de los elementos de un subconjunto de v
+ * @return Devuelva una pareja de iteradores que apuntan a elementos de V, un 
+ * iterador de cada conjunto
+*/
 Auxiliar puntos_cercanos(City *v, const list<int>& c1, const list<int>& c2){
 
     list<int>::const_iterator it1 = c1.cbegin();
     list<int>::const_iterator it2 =  c2.cbegin();
     Auxiliar solucion(it1, it2);
 
-    ll distancia =  v[*(it1++)].dist(v[*(it2++)]);
+    ll distancia =  v[*(it1)].dist(v[*(it2++)]);
 
     ll aux;
     for (; it1 != c1.cend(); ++it1) {
@@ -55,7 +70,11 @@ Auxiliar puntos_cercanos(City *v, const list<int>& c1, const list<int>& c2){
 }
 
 /**
+ * @brief Avanza o retrocede el iterador según @pos_relativa
  * @param pos_relativa si 1 -> accede al siguiente, si -1 -> accede al anterior
+ * @param c lista al cual pertenece el iterador @p it
+ * @param it iterador con la que se manipula 
+ * @return retorna un iterador que apunta a la posición a la que se quiere avanzar
 */
 list<int>::const_iterator acceso(int pos_relativa, const list<int>& c, list<int>::const_iterator it) {
 
@@ -90,7 +109,16 @@ list<int>::const_iterator acceso(int pos_relativa, const list<int>& c, list<int>
     return resultado;
 }
 
-// [init, fin)
+/**
+ * @brief Encuentra una aproximación del ciclo con valor mínimo que se puede establecer
+ * con las ciudades de @p v .El valor de un ciclo es la suma de las distancias a recorrer para 
+ * pasar por todas las ciudades en ciclo. 
+ * @param v vector que contiene a las ciudades
+ * @param init el primer elemento que se tiene en cuenta del vector
+ * @param fin el último elemento que se tiene en cuenta del vector
+ * @param ciclo lista donde se va a almacenar el orden en que se va a recorrer las ciudades 
+*/
+
 void dyv (City *v, int init, int fin, list<int>& ciclo) {
 
     if (fin - init < 3) {
@@ -107,15 +135,17 @@ void dyv (City *v, int init, int fin, list<int>& ciclo) {
     dyv(v, init, mid, c1);
     dyv(v, mid, fin, c2);
 
-    cout << "\nC1: " << c1;
-    cout << "\nC2: " << c2;
+//    cout << "\nC1: " << c1;
+//    cout << "\nC2: " << c2;
 
     // Combinar las soluciones 
 
     // Sacar los puntos a distancia mínima 
     Auxiliar puntos_dist_min = puntos_cercanos(v, c1, c2); 
 
-    // De los nodos consecutivos de los dos puntos, sacar un segundo par de puntos
+    // De los nodos adyacentes a los dos nodos seleccionados, sacar un segundo par de nodos con 
+    // distancia mínima
+
     int pos[] = {-1,1};
 
     list<int>::const_iterator aux_it1 = acceso(pos[0], c1, puntos_dist_min.it1);
@@ -124,8 +154,8 @@ void dyv (City *v, int init, int fin, list<int>& ciclo) {
     Auxiliar otros_puntos_dist_min(aux_it1, aux_it2);
     ll dist_aux = v[*aux_it1].dist(v[*aux_it2]);
 
-    for (int i = 1; i < 2; ++i) {
-        for(int j = 1; j < 2; ++j) {
+    for (int i = 0; i < 2; ++i) {
+        for(int j = 0; j < 2; ++j) {
             aux_it1 = acceso(pos[i], c1, puntos_dist_min.it1);
             aux_it2 = acceso(pos[j], c2, puntos_dist_min.it2);
 
@@ -136,11 +166,7 @@ void dyv (City *v, int init, int fin, list<int>& ciclo) {
         }
     }
 
-    // Insertar un ciclo en otro
-
-
-    cout << "\nElegidos_1: " << puntos_dist_min.to_s();
-    cout << "\nElegidos_2: " << otros_puntos_dist_min.to_s();
+    // Insertar un ciclo en otro/combinar los ciclos 
 
     // Ciclos a combinar son los indicados en puntos_dist_min y otros_puntos_dist_min
 
@@ -156,14 +182,14 @@ void dyv (City *v, int init, int fin, list<int>& ciclo) {
     // Hasta que no se hayan insertado todos los elementos 
     // Hasta que el iterador no de el ciclo entero
     // Ambas condiciones son equivalentes
-    // Es por debuguo 
+    // Es por debugueo 
 
     do {
         ciclo.push_back(*it_fusion);
         it_fusion = acceso(sentido, c1, it_fusion);
     } while (insertados < c1.size() && it_fusion != puntos_dist_min.it1);
 
-    cout << "\nInsertado_fase1: " << ciclo;
+ //   cout << "\nInsertado_fase1: " << ciclo;
 
     // Se inserta la siguiente lista
 
@@ -180,7 +206,7 @@ void dyv (City *v, int init, int fin, list<int>& ciclo) {
 
     } while (insertados < c2.size() && it_fusion != otros_puntos_dist_min.it2);
 
-   cout << "\nInsertado_fase2: " << ciclo;
+ //  cout << "\nInsertado_fase2: " << ciclo;
 }
 
 int main() {
@@ -195,22 +221,50 @@ int main() {
         cin >> vector[i];
     }
     
-    // sort(vector, vector+n); Falta definir el operador relacional  
-
+    sort(vector, vector+n);         //Falta definir el operador relacional  
+/*
     for (int i = 0; i < n; i++) {
         cout << vector[i] << " ";
     }
-
+*/
 
     list<int> ciclo_resultado;
 
     dyv(vector, 0, n, ciclo_resultado);
 
-    cout << "\nSe imprimirá el ciclo obtenido ( size = " << ciclo_resultado.size() << "): \n";
+    //  cout << "\nSe imprimirá el ciclo obtenido ( size = " << ciclo_resultado.size() << "): \n";
 
-    cout << ciclo_resultado;
+    // Girar ciclo_resultado hasta cero / ciudad inicial por defecto
+    
+    list<int> rotado;
+
+    typename list<int>::iterator it = ciclo_resultado.begin();
+
+    while (*it != 0) {
+        rotado.push_back(*it);
+        ++it;
+    }
+
+    rotado.insert(rotado.begin(), it, ciclo_resultado.end());
+
+    cout << rotado;
+    cout << *rotado.begin();
 
     cout << endl;
+    
+    double sum = 0;
+    typename list<int>::const_iterator pre  = rotado.cbegin();
+    typename list<int>::const_iterator post = pre;
+    ++post;
+
+    for (; post != rotado.cend(); ++post) {
+        sum += vector[*post].dist(vector[*(pre)]);
+        pre = post;
+    }
+
+    cout << sum;
+
+    return 0;
 }
 
 ostream& operator<<(ostream& os,const list<int>& l) {
