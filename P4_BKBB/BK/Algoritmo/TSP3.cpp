0d612bd92473057e3d82b82e5928db6039fa884b
Olga <cchengcheng2004@gmail.com>
2024-05-15 22:59:37 +0200

    Completado backtraking funcion cota1

--- P4_BKBB/BK/Algoritmo/TSP3.cpp
+++ P4_BKBB/BK/Algoritmo/TSP3.cpp
@@ -4,31 +4,65 @@
 
 using namespace std;
 
+#ifndef TIME 
+    #ifndef COST
+        #define TSP
+    #endif
+#endif
+
+
+// operator << for vector<City>
+template <typename T>
+ostream& operator<<(ostream& os, vector<T> v) {
+    for (int i=0; i < v.size(); i++) {
+        os << v[i] << endl;
+    }
+    return os;
+}
+
+template<typename T>
+void remove(vector<T> & v, const T & elem) {
+    auto it = find(v.begin(), v.end(), elem); 
+    if (it != v.end()) { 
+        v.erase(it); 
+    }
+}
 
 class TSP_solution
 {
     vector<int> best_ans;
     double cost; 
 
+    vector<City> cities;
     public: 
 
     TSP_solution(){};
 
-    vector<int> solve(vector<City>& cities)
+
+    double getCost() const {
+        return cost;
+    }
+
+    vector<int> solve(vector<City>& v)
     {   
         // Se limpia
         best_ans.clear();
         cost = 0;
 
+        cities = v;
+
         if (cities.empty()) return best_ans;
         
+        // Se empieza desde el origen para pasar de n! a (n-1)!
         vector<int> track;
-        track.push_back(0);                 // Se empieza desde el origen 
+        track.push_back(0);                 
         vector<bool> visited(cities.size(), false);
         visited[0] = true;
 
-        // Se puede llamar a greedy antes para podar 
-        cost = -1;
+        // Estable la primera cota 
+        TSP_greedy_v1(cities.size(), 0, cities, best_ans);
+        cost = cycleDistance(best_ans, cities);
+
         backtracking(cities, track, visited);
         return best_ans; 
     }
@@ -39,7 +73,7 @@ class TSP_solution
         {
             // Keep the best
             double cost_aux = cycleDistance(track, cities);
-            if (cost == -1 || cost_aux < cost) {
+            if (cost_aux < cost) {
                 best_ans = track;
                 cost = cost_aux;
             }
@@ -49,10 +83,8 @@ class TSP_solution
         // La primera ciudad se ignora
         for (int i=1; i < cities.size(); ++i)
         {
-            if (visited[i]) continue;
-            //if (visited[i] == true || podar(cities, track, visited, i)) continue;
-            
-            // Meter funcion podar segun el valor de la cota local
+            //if (visited[i]) continue;
+            if (visited[i] == true || podar(cities, track, visited, i)) continue;
 
             track.push_back(i);
             visited[i] = true;
@@ -60,19 +92,6 @@ class TSP_solution
             track.pop_back();
             visited[i] = false;
         }
-    }
-
-    double enter_min_cost(vector<City>& cities, vector<bool>& visited, int node)
-    {   
-        double min_enter = -1;
-        for(int i=0; i < visited.size(); ++i)
-        {
-            if (!visited[i] && ((min_enter == -1) || cities[node].dist(cities[i]) < min_enter))
-            {
-                min_enter = cities[node].dist(cities[i]);
-            }
-        }
-        return min_enter;
     }
 
     bool podar(vector<City>& cities, vector<int>& track, vector<bool>& visited, int node)
@@ -85,7 +104,7 @@ class TSP_solution
             cota_inf += (cities[track[i+1]] - cities[track[i]]);
         }
         
-        cota_inf += cities[node] - cities.back();
+        cota_inf += (cities[node] - cities[track.back()]);
 
         for (int i=0; i < cities.size(); ++i)
         {
@@ -93,6 +112,9 @@ class TSP_solution
                 cota_inf += enter_min_cost(cities, visited, i);
             }
         }
+        
+        // Coste para volver desde una ciudad
+        cota_inf +=enter_min_cost(cities, visited, 0);
 
         return (cota_inf >= cost);
     }
@@ -133,9 +155,64 @@ class TSP_solution
         std::cout << origin << std::endl;
     }
 
-    double getCost() const {
-        return cost;
+    private:
+
+    double enter_min_cost(vector<City>& cities, vector<bool>& visited, int node)
+    {   
+        double min_enter = -1;
+        for(int i=0; i < visited.size(); ++i)
+        {
+            if (i == node) continue;
+            if (!visited[i] && ((min_enter == -1) || cities[node].dist(cities[i]) < min_enter))
+            {
+                min_enter = cities[node].dist(cities[i]);
+            }
+        }
+        return min_enter;
     }
+
+    /**
+     * @brief Greedy approximated solution to the Travelling Salesman Problem 
+     * 
+     * @param n number of cities to visit
+     * @param home_ind index of the home (origin) city 
+     * @param v array of cities to visit
+     * @param path current path
+    */
+    void TSP_greedy_v1(int n, int home_ind, vector<City>& v, vector<int> & path){
+
+        // Not visited cities
+        vector<int> not_visited(n);
+
+        // Initializes with all cities
+        for(int i=0; i < n; i++) {
+            not_visited[i] = i;
+        }
+        // Start with home city
+        int current = home_ind;
+
+        while (not_visited.size() > 0) {
+            path.push_back(current);
+            remove(not_visited,current);
+
+            // Calculate the closest neighbour
+            ld min_dist = INF, dist;
+            int next;
+            for (int city : not_visited) {
+                dist = v[current]-v[city];
+                if (dist < min_dist) {
+                    min_dist = dist;
+                    next = city;
+                }
+            }
+
+            // Next city
+            current = next;
+        }
+        path.push_back(home_ind);
+    }
+
+    
 };
 
 int main(){
@@ -158,12 +235,24 @@ int main(){
 
     TSP_solution sol;
 
+    // TSP
+
+    clock_t t_before = clock();
     sol.solve(v);
+    clock_t t_after = clock();
 
     // OUTPUT
-    sol.printCycle(); 
+    #ifdef TSP
+    sol.printCycle();
+    #endif
 
-    cout << endl << n << " " << sol.getCost()<< endl;
+    #ifdef COST
+    cout << n << " " << sol.getCost() << endl;
+    #endif
+
+    #ifdef TIME
+    cout << n << " " << ((double)(t_after - t_before)/ CLOCKS_PER_SEC) << endl;
+    #endif
 
     return 0;
 }
